f739480186473c58a8c78ed62328dafc4efe2557
Afonso Garcia <afonsogfrgarcia@gmail.com>
2024-10-04 13:58:56 +0100

    Fix loop errors for oauth token and bump version

--- custom_components/bluecon/ConfigFolderOAuthTokenStorage.py
+++ custom_components/bluecon/ConfigFolderOAuthTokenStorage.py
@@ -1,3 +1,4 @@
+import asyncio
 from bluecon import IOAuthTokenStorage, OAuthToken
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.storage import Store
@@ -6,10 +7,15 @@ from pathlib import Path
 
 class ConfigFolderOAuthTokenStorage(IOAuthTokenStorage):
     def __init__(self, hass: HomeAssistant):
+        self.__hass = hass
         self.__oAuthTokenStore = Store(hass=hass, version=1, key=f"{DOMAIN}.OAUTH_TOKEN")
     
     async def retrieveOAuthToken(self) -> OAuthToken:
-        return OAuthToken.fromJson(await self.__oAuthTokenStore.async_load())
+        return OAuthToken.fromJson(asyncio.run_coroutine_threadsafe(
+            self.__oAuthTokenStore.async_load(), self.__hass.loop
+        ).result())
     
     async def storeOAuthToken(self, oAuthToken: OAuthToken):
-        await self.__oAuthTokenStore.async_save(oAuthToken.toJson())
+        asyncio.run_coroutine_threadsafe(
+            self.__oAuthTokenStore.async_save(oAuthToken.toJson()), self.__hass.loop
+        ).result()
